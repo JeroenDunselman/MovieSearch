1036ad1c0cb643f965315dc23068064db4bfd6da
Jeroen Dunselman <jeroduns@gmail.com>
2016-05-08 19:59:01 +0200

    Launch added

---
+++ SearchMovie/LaunchScreen.h
@@ -0,0 +1,13 @@
+//
+//  LaunchScreen.h
+//  SearchMovie
+//
+//  Created by Jeroen Dunselman on 08/05/16.
+//  Copyright © 2016 Jeroen Dunselman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchScreen : UIStoryboard
+
+@end
---
+++ SearchMovie/LaunchScreen.m
@@ -0,0 +1,13 @@
+//
+//  LaunchScreen.m
+//  SearchMovie
+//
+//  Created by Jeroen Dunselman on 08/05/16.
+//  Copyright © 2016 Jeroen Dunselman. All rights reserved.
+//
+
+#import "LaunchScreen.h"
+
+@implementation LaunchScreen
+
+@end
